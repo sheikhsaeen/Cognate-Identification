f28de0492aaf6d45fff13f07d735113812da6140
ckonst <christian.konstantinov98@gmail.com>
2021-03-15 22:38:57 -0400

    started loaded data in for baseline

--- code/baseline.py
+++ code/baseline.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
 """
-@authors
-sheikhsaeen
-ckonst
-"""
+Created on Mon Mar 15 21:21:15 2021
 
-def load_dataset(path):
-    pass
+@author: Christian Konstantinov
+"""
+import pandas as pd
+import epitran
+
+DATASET_PATH = '../data/CogNet-v2.0.tsv'
+SUPPORTED_LANGS_PATH = '../data/epitran_supported_langs.tsv'
+
+df = pd.read_csv(DATASET_PATH, sep='\t', header=1, error_bad_lines=False)
+langs = pd.read_csv(SUPPORTED_LANGS_PATH, sep='\t', header=0, error_bad_lines=False)
